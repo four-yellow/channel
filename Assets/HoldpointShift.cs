23eaa6567733538a261e9bc2828e7df261b19723
Kla368 <55667208+Kla368@users.noreply.github.com>
2019-10-06 15:03:05 -0400

    firstCommit

---
+++ Assets/HoldpointShift.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldpointShift : MonoBehaviour
+{
+    public float horizontal;
+
+    private void Start()
+    {
+        horizontal = GameObject.Find("Player").GetComponent<PlayerMove>().horDir;
+        transform.localPosition = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        horizontal = GameObject.Find("Player").GetComponent<PlayerMove>().horDir;
+        transform.localPosition = new Vector3(horizontal, 0, 0);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float moveSpeed;
+    Vector2 movement;
+    public float minHeight, maxHeight;
+    public float minWidth, maxWidth;
+
+    public float horDir; //-1=left, 1=right
+    public float verDir; //-1=down, 1=up
+
+    private void Start()
+    {
+        horDir = 1;
+        verDir = 1;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        if (System.Math.Abs(movement.x) > 0)
+        {
+            horDir = movement.x;
+        }
+
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (System.Math.Abs(movement.y) > 0)
+        {
+            verDir = movement.y;
+        }
+
+        if (((transform.position.x <= minWidth) && (movement.x <= -1)) || ((transform.position.x >= maxWidth) && (movement.x >= 1)))
+        {
+            movement.x = 0;
+        }
+        if (((transform.position.y <= minHeight) && (movement.y <= -1)) || ((transform.position.y >= maxHeight) && (movement.y >= 1)))
+        {
+            movement.y = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
