77a92e226ca99ee2477d338f9b73f41064a14fb2
Kla368 <55667208+Kla368@users.noreply.github.com>
2019-10-08 00:32:32 -0400

    SecondCommit
    Working movement, pickup/drop keys(bugs), destroying walls with keys

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * Allows the player to move using the arrow keys or WASD. Sets a height and width limit so the player cannot move
+ * off the screen, and displays horizontal and vertical inputs with 1 and -1. 
+ */
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float moveSpeed;
-    Vector2 movement;
+    Vector2 movement; // tracks direction the player is trying to move in. 
+
     public float minHeight, maxHeight;
     public float minWidth, maxWidth;
 
@@ -23,21 +27,15 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        if (System.Math.Abs(movement.x) > 0)
-        {
-            horDir = movement.x;
-        }
-
         movement.y = Input.GetAxisRaw("Vertical");
-        if (System.Math.Abs(movement.y) > 0)
-        {
-            verDir = movement.y;
-        }
 
+        // tests if player is trying to move past width boundaries
         if (((transform.position.x <= minWidth) && (movement.x <= -1)) || ((transform.position.x >= maxWidth) && (movement.x >= 1)))
         {
             movement.x = 0;
         }
+
+        // tests if player is trying to move past height boundaries
         if (((transform.position.y <= minHeight) && (movement.y <= -1)) || ((transform.position.y >= maxHeight) && (movement.y >= 1)))
         {
             movement.y = 0;
@@ -46,6 +44,6 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime); // moves the player
     }
 }
---
+++ Assets/UseKey.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Implements playge usage of a key. Keys near a player can be picked up with E, with closer keys taking priority,
+ * and can be dropped by pressing E again. When near a destroyable wall while holding a key, the player can press E
+ * to destroy the wall, using up the key.
+ */
+public class UseKey : MonoBehaviour
+{
+    public bool grabbed; // whether the player is holding a key
+    public static bool unlockable; // whether the player can destroy a wall
+    public Transform holdpoint; // point where player holds the key
+    public List<GameObject> keysInRange; // all keys nearby player
+    public GameObject adjacentWall; // a wall to destroy near the player
+
+    private void Start()
+    {
+        keysInRange = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        adjacentWall = collision.gameObject;               // stores a wall if it enters the collider of the player
+
+        if (collision.CompareTag("grabbable"))             // adds each key in the vicinity to an array
+        {
+            keysInRange.Add(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Wall"))                  // if a wall is in range and the player has a key, sets unlockable to true
+        {
+            if (grabbed)
+            {
+                unlockable = true;
+            }
+            else
+            {
+                unlockable = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("grabbable"))             // removes keys that leave the player's range
+        {
+            keysInRange.Remove(collision.gameObject);
+        }
+
+        unlockable &= !collision.CompareTag("Wall");       // sets unlockable to false if no walls leave the player's range
+        adjacentWall = null;
+    }
+
+    private void Update()
+    {
+        float[] distances = new float[keysInRange.Count];  // find distances from player of each key in range
+        for (int i = 0; i < distances.Length; i++)
+        {
+            distances[i] = Vector3.Distance(keysInRange[i].transform.position, this.gameObject.transform.position);
+        }
+
+        float minDistance = 999999;                        // find key with minimum distance
+        int index = 0;
+        for (int i = 0; i < distances.Length; i++)
+        {
+            if (distances[i] < minDistance)
+            {
+                minDistance = distances[i];
+                index = i;
+            }
+        }
+
+        if (distances.Length > 0)                          // checks that there are keys near the player     
+        {
+            GameObject closestObj = keysInRange[index];
+            if (Input.GetKeyDown(KeyCode.E))               // grabs/drops the key when E is pressed
+            {
+                if (!grabbed)
+                {
+                    grabbed = true;
+                }
+                else
+                {
+                    grabbed = false;
+                }
+            }
+
+            if (grabbed)                                   // moves key to the holdpoint of the player
+            {
+                closestObj.transform.position = holdpoint.position;
+            }
+
+            if (unlockable && Input.GetKeyDown(KeyCode.E)) // destroys a wall and uses up key when E is pressed if unlockable
+            {
+                Destroy(adjacentWall);
+                Destroy(closestObj);
+            }
+        }
+    }
+}
