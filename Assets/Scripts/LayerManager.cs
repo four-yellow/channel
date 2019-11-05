dea5f96688483dd19bdb55544eddda23a36a288d
RedHippi <32603448+RedHippi@users.noreply.github.com>
2019-11-04 22:38:22 -0500

    LayerManagement
    Partially works!!! Time to start working on more integration.

---
+++ Assets/Scripts/LayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : MonoBehaviour
+{
+    private SpriteRenderer MySR;
+
+    private int MyY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MySR = this.GetComponent<SpriteRenderer>();
+        Vector3 tmp = this.transform.position;
+        MyY = -(int)tmp.y;
+        MySR.sortingOrder = MyY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tmp = this.transform.position;
+        MyY = -(int)tmp.y;
+        MySR.sortingOrder = MyY;
+    }
+}
