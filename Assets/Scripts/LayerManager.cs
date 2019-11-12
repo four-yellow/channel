2b8446e61d0bef43050929efa4f4c97f04fc8865
RedHippi <32603448+RedHippi@users.noreply.github.com>
2019-11-12 10:50:51 -0500

    revamped art scene

--- Assets/Scripts/LayerManager.cs
+++ Assets/Scripts/LayerManager.cs
@@ -7,20 +7,30 @@ public class LayerManager : MonoBehaviour
     private SpriteRenderer MySR;
 
     private int MyY;
+
+    [Range(-1.0f,1.0f)]
+    public float Adjust;
+    private float AdjustCenter;
+    private float Fidelity = 1000;
+    private float height;
     // Start is called before the first frame update
     void Start()
     {
+        Bounds b = this.GetComponent<SpriteRenderer>().sprite.bounds;
+        height = b.max.y - b.min.y;
+        AdjustCenter = (Adjust*(this.transform.lossyScale.y * height)/2);
         MySR = this.GetComponent<SpriteRenderer>();
         Vector3 tmp = this.transform.position;
-        MyY = -(int)tmp.y;
+        MyY = -(int)((AdjustCenter+tmp.y)*Fidelity);
         MySR.sortingOrder = MyY;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AdjustCenter = (Adjust * (this.transform.lossyScale.y * height) / 2);
         Vector3 tmp = this.transform.position;
-        MyY = -(int)tmp.y;
+        MyY = -(int)((AdjustCenter+tmp.y) * Fidelity);
         MySR.sortingOrder = MyY;
     }
 }
