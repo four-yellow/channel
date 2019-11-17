e80d9dcb8cc787cee7d8be8f319c4fec70f8b535
Gli51 <47698133+Gli51@users.noreply.github.com>
2019-11-17 14:47:18 -0500

    tijalitweji
    waetaweti

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,70 +4,41 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public GameObject player;
-    public float smoothTime = 1F;
-    private Vector3 velocity = Vector2.zero;
-
-    public Transform LeftWall;
-    public Transform RightWall;
-    public Transform UpWall;
-    public Transform DownWall;
-
     public float xMin;
     public float xMax;
     public float yMin;
     public float yMax;
 
-    public float cameraOffset;
-    public float ycameraOffset;
+    private Vector3 pos;
+    private Transform target;
+    private SpriteRenderer spriteBounds;
 
     void Start()
     {
-        ycameraOffset = cameraOffset / 1.78f;
-        xMin = LeftWall.position.x + cameraOffset;
-        xMax = RightWall.position.x - cameraOffset;
-        yMin = DownWall.position.y + ycameraOffset;
-        yMax = UpWall.position.y - ycameraOffset;
+        transform.position = GameObject.FindWithTag("Player").transform.position;
+
+        float vertExtent = GetComponent<Camera>().orthographicSize;
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+
+        spriteBounds = GameObject.FindWithTag("background").GetComponentInChildren<SpriteRenderer>();
+
+        target = GameObject.FindWithTag("Player").transform;
+
+        //xMin = horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f;
+        xMax = spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent;
+        xMin = -1 * xMax;
+        yMin = vertExtent - spriteBounds.sprite.bounds.size.y / 2.0f;
+        yMax = spriteBounds.sprite.bounds.size.y / 2.0f - vertExtent;
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        if (player.transform.position.y < yMin && player.transform.position.x < xMin) // bottom left corner
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMin, yMin, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.y < yMin && player.transform.position.x > xMax) // bottom right corner
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMax, yMin, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.y > yMax && player.transform.position.x < xMin) // top left corner
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMin, yMax, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.y > yMax && player.transform.position.x > xMax) // top right corner 
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMax, yMax, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.y < yMin) // not corners
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, yMin, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.y > yMax)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, yMax, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.x < xMin)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMin, player.transform.position.y, -6), ref velocity, smoothTime);
-        }
-        else if (player.transform.position.x > xMax)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(xMax, player.transform.position.y, -6), ref velocity, smoothTime);
-        }
-        else
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, -6), ref velocity, smoothTime);
-
-        }
+        //Debug.Log();
+        pos = new Vector3(target.position.x, target.position.y, -6);
+        pos.x = Mathf.Clamp(pos.x, xMin, xMax);
+        pos.y = Mathf.Clamp(pos.y, yMin, yMax);
+        transform.position = pos;
     }
+
 }
