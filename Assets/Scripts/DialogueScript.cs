f1835858551270d6d8c691d83cac7322dce76694
Kla368 <55667208+Kla368@users.noreply.github.com>
2019-11-22 01:47:15 -0500

    Dialogue System
    Implement dialogue by placing all three prefabs in the dialogue folder into the scene. Input sentences into the Dialogue Manger, click continue to move through sentences. For now, Space bar begins the dialogue wherever you placed it in the scene. A sample is implemented in scene 1a (try pressing space after play)

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+    public float typingSpeed = 0.5f;
+    public TextMeshProUGUI dialogueText; //text displayed in the bubble
+
+    public GameObject speechBubble;
+    public GameObject continueButton;
+
+    [TextArea]
+    public string[] sentences; // array of sentences
+
+    public int dialogueIndex; // which sentence you're on
+
+    //starts the speech bubble if you press space
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speechBubble.SetActive(true);
+            StartDialogue();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        StartCoroutine("TypeDialogue");
+    }
+
+    //prints out letters of a sentence one by one
+    private IEnumerator TypeDialogue()
+    {
+        foreach (char letter in sentences[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        continueButton.SetActive(true);
+    }
+
+    //increments which sentence you're on when you press continue, and closes the bubble if done
+    public void ContinueDialogue()
+    {
+        continueButton.SetActive(false);
+
+        if (dialogueIndex < sentences.Length - 1)
+        {
+            dialogueIndex++;
+            dialogueText.text = string.Empty;
+            StartCoroutine("TypeDialogue");
+        }
+        else
+        {
+            speechBubble.SetActive(false);
+        }
+    }
+}
