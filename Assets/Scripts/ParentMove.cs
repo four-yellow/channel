58349647caf1af9766395f8f1cd4cfec06345602
RedHippi <32603448+RedHippi@users.noreply.github.com>
2019-11-26 16:03:56 -0500

    Added new parent movement method based off of walls being removed

--- Assets/Scripts/ParentMove.cs
+++ Assets/Scripts/ParentMove.cs
@@ -8,23 +8,28 @@ public class ParentMove : MonoBehaviour
     public struct WayPoint
     {
         public GameObject game;
+        public GameObject[] walls;
         public bool bl;
     }
     private float speed = 3;
     public WayPoint[] parentMoveToXY;
     private float upBound, downBound, rightBound, leftBound;
     private int count;
-    private bool obstacleInWay; // raycast hit
+    //private bool obstacleInWay; // raycast hit
     private Vector3 direction;
-    public float rayDistance = 2f;
+    //public float rayDistance = 2f;
     public boolRef h;
 
     //We will condense our X,Y coordinates into a new struct (as well as record whether or not the parent
     //is hungry at the start of this path.
+
+    //A new value is being added to better decide when to move. An array will be given with walls.
+    //Once all of these are deactivated then our parent will move.
     struct Coord
     {
         public float x;
         public float y;
+        public GameObject[] obstacles;
         public bool startHungry;
     }
 
@@ -38,11 +43,19 @@ public class ParentMove : MonoBehaviour
         for (int i = 0; i < parentMoveToXY.Length; i++)
         {
             Vector3 v = parentMoveToXY[i].game.transform.position;
-            (Path[i].x, Path[i].y, Path[i].startHungry) = (v.x, v.y, parentMoveToXY[i].bl); 
+            (Path[i].x, Path[i].y, Path[i].startHungry) = (v.x, v.y, parentMoveToXY[i].bl);
+            Path[i].obstacles = parentMoveToXY[i].walls;
         }
 
         count = 0;
         h.Val = Path[count].startHungry;
+    }
+
+    private bool AllDisabled (GameObject[] objects)
+    {
+        foreach (GameObject g in objects) { if ((g.activeInHierarchy) == true) return false; }
+        return true;
+            
     }
 
     private void Update()
@@ -57,12 +70,12 @@ public class ParentMove : MonoBehaviour
 
             Vector3 checkpoint = new Vector3(Path[count].x, Path[count].y);
             direction = (checkpoint - transform.position).normalized;
-            obstacleInWay = Physics2D.Raycast(transform.position, direction, rayDistance);
+            //obstacleInWay = Physics2D.Raycast(transform.position, direction, rayDistance);
 
             if (transform.position.y >= upBound || transform.position.y <= downBound ||
                 transform.position.x >= rightBound || transform.position.x <= leftBound)
             {
-                if (!obstacleInWay)
+                if (AllDisabled(Path[count].obstacles) /*&& !obstacleInWay*/)
                     transform.Translate(direction * speed * Time.deltaTime);
             }
             else { count++;  if (count < parentMoveToXY.Length) h.Val = Path[count].startHungry; }
