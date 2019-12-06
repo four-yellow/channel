9e7640a5a6d6e4a4b33d0785a20f8362e25b8bf2
RedHippi <32603448+RedHippi@users.noreply.github.com>
2019-12-06 16:08:15 -0500

    Oh yeah
    Take that dom no conflicts loser

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private Animator Mine;
     public float moveSpeed;
     Vector2 movement; // tracks direction the player is trying to move in. 
 
@@ -19,8 +20,10 @@ public class PlayerMove : MonoBehaviour
     public float horDir; //-1=left, 1=right
     public float verDir; //-1=down, 1=up
 
+    private int direction;
     private void Start()
     {
+        Mine = GetComponent<Animator>();
         horDir = 1;
         verDir = 1;
         rb2d = GetComponent<Rigidbody2D>();
@@ -30,7 +33,29 @@ public class PlayerMove : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        if ((movement.magnitude) <= 0.0f) Mine.SetBool("Walking", false);
+        else
+        {
+            Mine.SetBool("Walking", true);
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                direction = 3;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                direction = 0;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                direction = 2;
+            }
+            Mine.SetInteger("Direction", direction);
+        }
+            
         //I don't think these boundaries are necessary
         /*
         // tests if player is trying to move past width boundaries
